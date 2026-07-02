66c9886d27125e1c50373f51a7ab01b9f0635f00
Kareemgomaa <kareem.gomaa.2001@gmail.com>
2026-07-02 13:40:15 +0300

    add mongoDB and fix bugs

--- apps/chat/src/presentation/controllers/chat-rpc.controller.ts
+++ apps/chat/src/presentation/controllers/chat-rpc.controller.ts
@@ -1,13 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import type { IMessageRepository } from '../../application/ports/message-repository.interface';
+import { Message } from '../../domain/entities/message.entity';
 
 @Controller()
 export class ChatRpcController {
-  @MessagePattern('msg_sent')
-  handleMessageSent(@Payload() data: any) {
+  constructor(
+    @Inject('IMessageRepository')
+    private readonly messageRepository: IMessageRepository,
+  ) {}
+
+  @EventPattern('msg_sent')
+  async handleSendMessage(@Payload() data: any) {
     console.log(
       '[Chat Microservice] Received a new message event via RabbitMQ:',
+      data,
     );
-    console.log(data);
+
+    try {
+      const messageEntity = new Message({
+        tripId: data.tripId,
+        senderId: data.senderId,
+        message: data.message,
+        location: data.location,
+        seen: false,
+      });
+
+      const savedMessage = await this.messageRepository.save(messageEntity);
+
+      console.log(
+        'Message successfully saved to MongoDB Atlas with ID:',
+        savedMessage.id,
+      );
+    } catch (error) {
+      console.error('Error saving message to MongoDB Atlas:', error.message);
+    }
   }
 }
